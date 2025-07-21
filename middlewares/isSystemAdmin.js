656922efa3717c4689b14a2d68de84a38bf2e200
kholif18 <rahmadnur.kholifatur@gmail.com>
2025-07-21 16:26:30 +0700

    pull

--- middlewares/isSystemAdmin.js
+++ middlewares/isSystemAdmin.js
@@ -1,9 +1,21 @@
 module.exports = function isSystemAdmin(req, res, next) {
     if (!req.session.user) {
+        // Jika tidak login
+        if (req.xhr || req.headers.accept.includes('application/json')) {
+            return res.status(401).json({
+                error: 'Unauthorized'
+            });
+        }
         return res.redirect('/login');
     }
 
     if (req.session.user.role !== 'admin') {
+        if (req.xhr || req.headers.accept.includes('application/json')) {
+            return res.status(403).json({
+                error: 'Access Denied: Admin Only'
+            });
+        }
+
         return res.status(403).render('errors/403', {
             layout: false,
             user: req.session.user,
