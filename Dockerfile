8f8a6ccac3613fbe00338283fcbd42989b24ed74
kholif18 <rahmadnur.kholifatur@gmail.com>
2025-07-24 14:37:43 +0700

    setup

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:20
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+RUN npm run copy-assets
+
+CMD ["npm", "start"]
---
+++ setup.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const {
+    execSync
+} = require('child_process');
+const path = require('path');
+
+const envPath = path.join(__dirname, '.env');
+const envExamplePath = path.join(__dirname, '.env.example');
+
+console.log('Menjalankan instalasi awal Waserva...');
+
+if (!fs.existsSync(envPath)) {
+    fs.copyFileSync(envExamplePath, envPath);
+    console.log('.env berhasil dibuat dari .env.example');
+} else {
+    console.log('.env sudah ada, dilewati.');
+}
+
+try {
+    execSync('npm install', {
+        stdio: 'inherit'
+    });
+    execSync('npm run copy-assets', {
+        stdio: 'inherit'
+    });
+    execSync('npx sequelize db:migrate', {
+        stdio: 'inherit'
+    });
+    execSync('npx sequelize db:seed:all', {
+        stdio: 'inherit'
+    });
+    console.log('\n🎉 Instalasi selesai! Jalankan `npm start` untuk memulai aplikasi.');
+} catch (err) {
+    console.error('Terjadi kesalahan saat instalasi:', err);
+}
